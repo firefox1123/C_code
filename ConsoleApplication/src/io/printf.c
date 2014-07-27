00762050d2e9943546a6a0a86e37be832e28f3fa
firefox1123 <zhawei_long@126.com>
2014-07-27 09:54:42 +0800

    add a note
    添加了笔记，关于各种常用函数的简介。

---
+++ ConsoleApplication/src/io/printf.c
@@ -0,0 +1,21 @@
+/* printf.c -- printf()函数的使用 */
+#include <stdio.h>
+#include <limits.h>
+#include <float.h>
+#include <string.h>
+#include <conio.h>
+
+int main(void)
+{
+	char vchar = 'a';
+	char vcharArray[] = "There is flower!";
+	short vshort;
+	int vint;
+	long vlong;
+	long long int vllong;
+	
+	float vfloat;
+	double vdouble;
+getch();
+
+}
